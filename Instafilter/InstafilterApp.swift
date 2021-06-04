d91e5771aee20b5d5eb34dc4d4fd99abf981a1c1
antonivegaochoa <tonivegaochoa@gmail.com>
2021-06-04 18:43:11 -0400

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Antonio Vega on 6/4/21.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
