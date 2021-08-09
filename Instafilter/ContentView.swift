d373790efc987d4a7f0c81462baa018b3c0a55d0
antonivegaochoa <tonivegaochoa@gmail.com>
2021-08-09 17:21:31 -0500

    Using coordinators to manage SwiftUI view controllers and saving images

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -9,9 +9,20 @@ import SwiftUI
 import CoreImage
 import CoreImage.CIFilterBuiltins
 
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished!")
+    }
+}
+
 struct ContentView: View {
     @State private var image: Image?
     @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
 
     var body: some View {
         VStack {
@@ -23,31 +34,18 @@ struct ContentView: View {
                 showingImagePicker = true
             }
         }
-        .sheet(isPresented: $showingImagePicker) {
-            ImagePicker()
+        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: $inputImage)
         }
     }
     
     func loadImage() {
-        guard let inputImage = UIImage(named: "example") else { return }
-        let beginImage = CIImage(image: inputImage)
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
         
-        let context = CIContext()
-        guard let currentFilter = CIFilter(name: "CITwirlDistortion") else { return }
-        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-        currentFilter.setValue(2000, forKey: kCIInputRadiusKey)
-        currentFilter.setValue(CIVector(x: inputImage.size.width / 2, y: inputImage.size.height / 2), forKey: kCIInputCenterKey)
-        
-//        currentFilter.inputImage = beginImage
-//        currentFilter.intensity = 1
-        
-        guard let outputImage = currentFilter.outputImage else { return }
-        
-        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-            let uiImage = UIImage(cgImage: cgimg)
-            
-            image = Image(uiImage: uiImage)
-        }
+//        UIImageWriteToSavedPhotosAlbum(inputImage, nil, nil, nil)
+        let imageSaver = ImageSaver()
+        imageSaver.writeToPhotoAlbum(image: inputImage)
     }
 }
 
